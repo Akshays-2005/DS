3348a148e6cfe38e47764fe7b39c24bdd3858da5
Akshays-2005 <akshays.cs23@bmsce.ac.in>
2024-10-31 11:33:22 +0530

    Add files via upload

---
+++ fibonacci_using_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int fibo(int n)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    else if(n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+void main()
+{
+    int n,fib;
+    printf("Enter number:");
+    scanf("%d",&n);
+    fib=fibo(n);
+    printf("Fibonacci number:%d",fib);
+}
