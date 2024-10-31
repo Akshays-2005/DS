826fec6ba03cf920ad23d9cc0701be9b619c5626
Akshays-2005 <akshays.cs23@bmsce.ac.in>
2024-10-31 11:38:22 +0530

    Update fibonacci_using_recursion.c

--- fibonacci_using_recursion.c
+++ fibonacci_using_recursion.c
@@ -1,3 +1,5 @@
+//Fibonacci number using recursion
+Code:
 #include<stdio.h>
 int fibo(int n)
 {
@@ -22,3 +24,60 @@ void main()
     fib=fibo(n);
     printf("Fibonacci number:%d",fib);
 }
+Output:
+//Fibonacci number using recursion
+Code:
+#include<stdio.h>
+int fibo(int n)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    else if(n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+void main()
+{
+    int n,fib;
+    printf("Enter number:");
+    scanf("%d",&n);
+    fib=fibo(n);
+    printf("Fibonacci number:%d",fib);
+}
+Output:
+//Fibonacci number using recursion
+Code:
+#include<stdio.h>
+int fibo(int n)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    else if(n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+void main()
+{
+    int n,fib;
+    printf("Enter number:");
+    scanf("%d",&n);
+    fib=fibo(n);
+    printf("Fibonacci number:%d",fib);
+}
+Output:
+Enter number:8
+Fibonacci number:13
