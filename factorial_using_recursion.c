79dd799b28fc3668e76ddc29cb2e4335de507227
Akshays-2005 <akshays.cs23@bmsce.ac.in>
2024-10-31 11:30:58 +0530

    Add files via upload

---
+++ factorial_using_recursion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fact(int n)
+{
+    int f;
+    if(n==0 || n==1)
+    {
+        f=1;
+    }
+    else
+    {
+        f=n*fact(n-1);
+    }
+    return f;
+}
+void main()
+{
+    int n,t,f1;
+    printf("Enter number:");
+    scanf("%d",&n);
+    t=n;
+    f1=fact(n);
+    printf("Factorial of %d is %d",t,f1);
+}
